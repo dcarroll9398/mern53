89458e25bdd79bbf0d554069fffac501cd267046
dcarroll9398 <dcarroll9398@yahoo.com>
2022-05-10 10:10:53 -0500

    Video-12

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,16 @@ const app = express();
 app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
+app.get('/api/products/slug/:slug', (req, res) => {
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send('message: Product Not Found');
+  }
+});
 
-const port = process.nextTick.PORT || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
 });
