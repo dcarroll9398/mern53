73f727365ac696901194ef25edf21ed0551e661f
dcarroll9398 <dcarroll9398@yahoo.com>
2022-05-26 09:31:29 -0500

    Video-13

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.get('/api/products/slug/:slug', (req, res) => {
   if (product) {
     res.send(product);
   } else {
-    res.status(404).send('message: Product Not Found');
+    res.status(404).send({ message: 'Product Not Found' });
   }
 });
 
