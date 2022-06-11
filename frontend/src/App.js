b3a65d44b2d620080faa3908ee11efa00e91f6ca
dcarroll9398 <dcarroll9398@yahoo.com>
2022-06-11 16:36:20 -0500

    Video-25

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
+import SignupScreen from './screens/SignupScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -72,6 +73,7 @@ function App() {
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
               <Route path="/shipping" element={<ShippingAddressScreen />} />
+              <Route path="/signup" element={<SignupScreen />} />
             </Routes>
           </Container>
         </main>
